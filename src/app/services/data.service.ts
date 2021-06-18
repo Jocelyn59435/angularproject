c741fbffe211479b182bd9a8be9dcbe436cd9f83
unknown <jocelynhuang193@outlook.com>
2021-06-18 10:09:38 +1000

    update services & README

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { UserInfo } from '../models/UserInfo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  userInfo: UserInfo = { address: '', first_name: '', total_cost: 0 };
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<[]> {
+    return this.http.get<[]>('assets/data.json');
+  }
+
+  updateUserInfo(currentUserInfo: UserInfo) {
+    this.userInfo = currentUserInfo;
+  }
+
+  getUserInfo() {
+    return this.userInfo;
+  }
+}
